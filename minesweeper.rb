512567bd8f82f8b9219a60c75fc965004aa79d23
Leah Drake <drake.leah@gmail.com>
2015-08-28 14:11:10 -0700

    Finish populating board

--- minesweeper.rb
+++ minesweeper.rb
@@ -16,6 +16,7 @@ class Board
     @grid = Array.new(side_length) { Array.new(side_length) }
     populate_bombs
     populate_non_bombs
+    p @grid
   end
 
   def populate_bombs
@@ -26,19 +27,25 @@ class Board
       bomb_positions.uniq!
       bomb_positions = bomb_positions.take(10)
     end
-    p bomb_positions
     # (side_length * 2).times do |x|
 
     #put tile instance in each position
     bomb_positions.each do |coordinates|
       @grid[coordinates[0]][coordinates[1]] = Tile.new(@board, bombed: true)
+      # p @grid[coordinates[0]][coordinates[1]]
+      # @grid[0][0]
 
-      @grid [0][0]
+      # output = @grid.map {|row| row.each {|col| col.inspect}}
+      # puts output
     end
   end
 
   def populate_non_bombs
-
+    @grid.each_with_index do |x,i|
+      x.each_with_index do |y,o|
+        @grid[i][o] = Tile.new(@board) if y.nil?
+      end
+    end
   end
 
   #9x9 grid
@@ -48,7 +55,7 @@ end #end of Board class
 
 
 class Tile
-  #bombed, flagged, revealed, neighbors
+  attr_accessor :options#:bombed, :flagged, :revealed, :neighbors, :value,
 
 
   def initialize(board, options = { })
@@ -56,22 +63,41 @@ class Tile
     defaults = {
       flagged: false,
       revealed: false,
-      bombed: false
+      bombed: false,
+      value: 0
     }
 
-    options = defaults.merge(options)
-
+    @tile_properties = defaults.merge(options)
+    if !@tile_properties[:bombed] #check neighbors and assign value
     # flag if flagged
     # bomb if bombed
     # reveal if revealed
+    end
 
   end
 
-  #methods reveal, neighbords, neighbor bomb count, flag, bomb,
+  #methods reveal, neighbors, neighbor bomb count, flag, bomb,
 
-  def inspect
+  def inspect #to make p method work
     #only show what we want
+    #if it's flagged, show 'U0569' = flag
+    #if it's revealed, show value
+    return nil if self.is_a?(Array) || self.nil?
+    if @tile_properties[:revealed]
+      if @title_properties[:bombed]
+        return "\u00A5".encode('utf-8')
+      else
+        return @tile_properties[:value]
+      end
+    end
+
+    if @title_properties[:flagged]
+      "\u0569".encode('utf-8')
+    else
+      "\u047A".encode('utf-8')
+    end
   end
+
 end
 
 #-------------------------------------------------
